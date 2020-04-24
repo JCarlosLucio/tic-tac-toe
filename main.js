31d5d6be13e2e06f8d992ae7807846239c443604
jcLucio <c.lucio3@gmail.com>
2020-04-24 12:02:36 -0500

    Add playerFactory

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const gameBoard = (() => {
 	const board = [];
 })();
+
+const playerFactoy = (name, mark) => {
+	const ready = () => {
+		console.log(`${name} has chosen ${mark}`);
+	};
+	return ready;
+};
