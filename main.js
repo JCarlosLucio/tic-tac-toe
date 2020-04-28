e853120e5f4499de86828000e8cf9512ed47f327
jcLucio <c.lucio3@gmail.com>
2020-04-28 11:09:57 -0500

    Add ganeOver logic

--- main.js
+++ main.js
@@ -5,10 +5,31 @@ const gameBoard = (() => {
 		for (let i = 0; i < board.length; i++) {
 			document.getElementById(`${i}`).textContent = board[i];
 			document.getElementById(`${i}`).addEventListener('click', (e) => {
-				console.log(e.target);
+				// console.log(e.target);
 				playerOne.placeMark(e.target);
-				PlayerTwo.placeMark(e.target);
+				playerTwo.placeMark(e.target);
 			});
+		}
+		// Check if win / tie
+		gameOver(board);
+	};
+	const gameOver = (board) => {
+		switch (true) {
+			case board[0] === board[1] && board[1] === board[2] && board[0] != '':
+			case board[0] === board[3] && board[3] === board[6] && board[0] != '':
+			case board[0] === board[4] && board[4] === board[8] && board[0] != '':
+			case board[1] === board[4] && board[4] === board[7] && board[1] != '':
+			case board[2] === board[4] && board[4] === board[6] && board[2] != '':
+			case board[2] === board[5] && board[5] === board[8] && board[2] != '':
+			case board[3] === board[4] && board[4] === board[5] && board[3] != '':
+			case board[6] === board[7] && board[7] === board[8] && board[6] != '':
+				console.log(`${gameBoard.turn === playerOne.mark ? playerOne.name : playerTwo.name} WON`);
+				break;
+			case !board.includes(''):
+				console.log('TIE');
+				break;
+			default:
+				console.log('WE ARE NOT DONE YET');
 		}
 	};
 	const turn = '';
@@ -20,7 +41,7 @@ const Player = (name = 'player1', mark = 'X') => {
 		console.log(`${name} has chosen ${mark}`);
 	};
 	const placeMark = (boardPlace) => {
-		console.log(boardPlace);
+		// console.log(boardPlace);
 		if (!gameBoard.board[Number(boardPlace.id)] && gameBoard.turn != mark) {
 			gameBoard.board[Number(boardPlace.id)] = mark;
 			gameBoard.turn = mark;
@@ -31,6 +52,6 @@ const Player = (name = 'player1', mark = 'X') => {
 };
 
 const playerOne = Player('John', 'X');
-const PlayerTwo = Player('Computer', 'O');
+const playerTwo = Player('Computer', 'O');
 
 gameBoard.render();
