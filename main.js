5feb14560d6f3a5d36f02b05e691c69cb9f31a39
jcLucio <c.lucio3@gmail.com>
2020-04-30 13:16:49 -0500

    Delete unused code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-	// let board = [ 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O' ];
+	const turn = '';
 	let playerOne;
 	let playerTwo;
 	let playerOneScoreCount = 0;
@@ -56,7 +56,6 @@ const gameBoard = (() => {
 		for (let i = 0; i < board.length; i++) {
 			document.getElementById(`${i}`).textContent = board[i];
 			document.getElementById(`${i}`).addEventListener('click', (e) => {
-				// console.log(e.target);
 				playerOne.placeMark(e.target);
 				playerTwo.placeMark(e.target);
 			});
@@ -92,26 +91,16 @@ const gameBoard = (() => {
 				break;
 		}
 	};
-	const turn = '';
-	return { board, start, render, turn };
+	return { board, render, turn };
 })();
 
 const Player = (name = 'player1', mark = 'X') => {
-	const chosenMsg = () => {
-		console.log(`${name} has chosen ${mark}`);
-	};
 	const placeMark = (boardPlace) => {
-		// console.log(boardPlace);
 		if (!gameBoard.board[Number(boardPlace.id)] && gameBoard.turn != mark) {
 			gameBoard.board[Number(boardPlace.id)] = mark;
 			gameBoard.turn = mark;
 			gameBoard.render();
 		}
 	};
-	return { name, mark, chosenMsg, placeMark };
+	return { name, mark, placeMark };
 };
-
-// const playerOne = Player('John', 'X');
-// const playerTwo = Player('Computer', 'O');
-
-// gameBoard.start();
